6eabb6313c71023a3aa14a59c91591627d8ecbdf
Aaron Prunty <aaron@Aarons-MacBook-Pro.local>
2018-09-02 10:52:22 -0600

    version update

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ with open('README.md', 'r') as fh:
      long_description = fh.read()
 
 setup(name = 'vezda',
-      version = '0.4.3',
+      version = '0.4.4',
       description = 'A set of command-line tools for imaging with the linear sampling method',
       long_description = long_description,
       long_description_content_type = 'text/markdown',
